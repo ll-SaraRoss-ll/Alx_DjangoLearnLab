bae451d6633ad390a4f8769856c2e7b5a28d3a3f
Sara <sararossmay2000@gmail.com>
2025-09-07 13:13:27 +0300

    Add role-based access control with UserProfile and restricted view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import admin_view, librarian_view, member_view
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
@@ -10,4 +11,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # <-- checker wants this
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # <-- checker wants this
     path('register/', views.register_view, name='register'),  # <-- checker wants views.register
+
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,6 +9,9 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import UserProfile
+
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
@@ -47,3 +50,28 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
