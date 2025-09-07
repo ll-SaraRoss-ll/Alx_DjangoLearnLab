588d26aa5fe44aaad910b37e056e28b5d503c270
Sara <sararossmay2000@gmail.com>
2025-09-07 13:46:11 +0300

    Complete role-based access and custom permissions for book management

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import list_books
 from .views import admin_view, librarian_view, member_view
 from .views import add_book, edit_book, delete_book
 
