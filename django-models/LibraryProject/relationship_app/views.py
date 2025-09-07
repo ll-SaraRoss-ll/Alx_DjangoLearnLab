75f786e38063ea9c512719a253039b6cfb0b956b
Sara <sararossmay2000@gmail.com>
2025-09-07 12:47:04 +0300

    Fix: use raltive import and Book.objects.all()

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from relationship_app.models import Book
+from django.views.generic.detail import DetailView
+from .models import Book, Library  
 
+# Function-based view to list all books
 def list_books(request):
-    books = Book.objects.all()
+    books = Book.objects.all() 
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-from django.views.generic.detail import DetailView
-from relationship_app.models import Library
-
+# Class-based view to show details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
