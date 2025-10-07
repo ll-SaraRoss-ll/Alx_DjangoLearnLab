c9405148ad9bef178deba9536dca7e2123bf53c4
Sara <sararossmay2000@gmail.com>
2025-10-07 19:46:04 +0300

    Deploying the Django Rest API to Production

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PORT = int(os.environ.get("PORT", os.environ.get("APP_PORT", 8000)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
