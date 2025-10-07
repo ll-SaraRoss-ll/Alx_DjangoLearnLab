cb6c659e3669f6141edce4ff7e3b74ac0623fb18
Sara <sararossmay2000@gmail.com>
2025-10-07 18:53:04 +0300

    Deploying the Django REST API to Production

--- social_media_api/manage.py
+++ social_media_api/manage.py
@@ -2,7 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
+load_dotenv()  # Load .env file
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings.production')
 
 def main():
     """Run administrative tasks."""
